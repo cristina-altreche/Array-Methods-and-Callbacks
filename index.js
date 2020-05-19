0c169aa50da520a96d725299f239c29ec53bca27
Cristina Altreche <c.altreche@gmail.com>
2020-05-18 19:26:34 -0500

    Task 3 complete

--- index.js
+++ index.js
@@ -22,20 +22,18 @@ const ht2014 = fifaData.filter((s) => {
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(arr) {
-  return arr.filter((f) => {
-    return f.Stage === "Final";
-  });
+  return arr.filter((f) => f.Stage === "Final");
 }
 
 console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-  /* code here */
+function getYears(callback) {
+  return callback.map((y) => y.Year);
 }
 
-getYears();
+console.log(getYears(getFinals(fifaData)));
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */
 
